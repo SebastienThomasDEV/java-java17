f70a2af4584e6b8f39506d6c152b6c8ba87b44c3
Sébastien THOMAS <74055963+SebastienThomasDEV@users.noreply.github.com>
2024-06-10 15:08:25 +0200

    tp 5

--- src/test/java/java17/ex02/Stream_02_Test.java
+++ src/test/java/java17/ex02/Stream_02_Test.java
@@ -10,9 +10,9 @@ import java.util.OptionalDouble;
 import org.junit.Test;
 
 import java17.data.Data;
-import java17.data.domain.Customer;
 import java17.data.domain.Order;
-
+import java17.data.domain.Pizza;
+import java17.data.domain.Customer;
 /**
  * Exercice 02 - Transformation
  */
@@ -24,7 +24,9 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Trouver la liste des clients associés aux commandes
-		List<Customer> result = null;
+		// exemple de référence de méthode : Order::getCustomer
+		// revient à écrire : o -> o.getCustomer()
+		List<Customer> result = orders.stream().map(Order::getCustomer).toList();
 
 		assertThat(result, hasSize(8));
 	}
@@ -35,7 +37,7 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Compter le nombre de clients associés aux commandes
-		long result = 0;
+		long result = orders.stream().map(Order::getCustomer).count();
 
 		assertThat(result, is(8L));
 	}
@@ -44,10 +46,8 @@ public class Stream_02_Test {
 	public void test_map_distinct() throws Exception {
 
 		List<Order> orders = new Data().getOrders();
-
 		// TODO Trouver la liste des différents clients associés aux commandes (sans doublons)
-		List<Customer> result = null;
-
+		List<Customer> result = orders.stream().map(Order::getCustomer).distinct().toList();
 		assertThat(result, hasSize(2));
 	}
 
@@ -57,8 +57,7 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Compter le nombre des différents clients associés aux commandes
-		long result = 0L;
-
+		long result = orders.stream().map(Order::getCustomer).distinct().count();
 		assertThat(result, is(2L));
 	}
 
@@ -71,8 +70,7 @@ public class Stream_02_Test {
 		 * TODO Calculer le chiffre d'affaires total de la pizzeria (somme des prix des
 		 * commandes)
 		 */
-		double result = 0.0;
-
+		double result = orders.stream().map(Order::getPrice).mapToDouble(Double::doubleValue).sum();
 		assertThat(result, is(10900.0));
 	}
 
@@ -84,7 +82,7 @@ public class Stream_02_Test {
 		/*
 		 * TODO Calculer le prix moyen d'une commande
 		 */
-		OptionalDouble result = null;
+		OptionalDouble result = orders.stream().map(Order::getPrice).mapToDouble(Double::doubleValue).average();
 
 		assertThat(result.isPresent(), is(true));
 		assertThat(result.getAsDouble(), is(1362.5));
--- src/test/java/java17/ex04/Stream_04_Test.java
+++ src/test/java/java17/ex04/Stream_04_Test.java
@@ -4,9 +4,14 @@ package java17.ex04;
 import org.junit.Test;
 
 import java.util.stream.Stream;
-
+import java17.data.Data;
+import java17.data.domain.Order;
+import java17.data.domain.Pizza;
+import java17.data.domain.Customer;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+import java17.data.Data;
+import java17.data.domain.*;
 
 /**
  * Exercice 04 - Stream
@@ -16,8 +21,7 @@ public class Stream_04_Test {
     @Test
     public void test_of() throws Exception {
         // Construire un stream permettant de rendre le test passant
-        Stream<String> result = null;
-
+        Stream<String> result = Stream.of("Alexandra", "Cyril", "Johnny", "Marion", "Sophie");
         assertThat(result.toArray(), arrayContaining("Alexandra", "Cyril", "Johnny", "Marion", "Sophie"));
     }
 
@@ -26,9 +30,14 @@ public class Stream_04_Test {
 
         // TODO compléter pour rendre le test passant
         // TODO utiliser la méthode "add"
-        Stream<Object> result = Stream.builder().build();
-
-        assertThat(result.toArray(), arrayContaining("Alexandra", "Cyril", "Johnny", "Marion", "Sophie"));
+        Stream.Builder<String> result = Stream.builder();
+        result.add("Alexandra");
+        result.add("Cyril");
+        result.add("Johnny");
+        result.add("Marion");
+        result.add("Sophie");
+        Stream<String> result2 = result.build();
+        assertThat(result2.toArray(), arrayContaining("Alexandra", "Cyril", "Johnny", "Marion", "Sophie"));
     }
 
     @Test
@@ -37,7 +46,7 @@ public class Stream_04_Test {
         Stream<String> s2 = Stream.of("Johnny", "Marion", "Sophie");
 
         // TODO concatener les deux streams s1 et s2
-        Stream<String> result = null;
+        Stream<String> result = Stream.concat(s1, s2);
 
         assertThat(result.toArray(), arrayContaining("Alexandra", "Cyril", "Johnny", "Marion", "Sophie"));
     }
@@ -45,8 +54,8 @@ public class Stream_04_Test {
     @Test
     public void test_iterate() throws Exception {
         // TODO utiliser la méthode "iterate" de Stream afin de rendre le test passant
-        Stream<Integer> result1 = null;
-        Stream<Integer> result2 = null;
+        Stream<Integer> result1 = Stream.iterate(1, i -> i).limit(5);
+        Stream<Integer> result2 = Stream.iterate(1, i -> i + 1).limit(5);
         assertThat(result1.toArray(), arrayContaining(1,1,1,1,1));
         assertThat(result2.toArray(), arrayContaining(1,2,3,4,5));
     }
